423d1e437bc63c5750e5dd4e2dae9a698126058c
Maxwell <krihkrihzo@gmail.com>
2026-07-31 18:08:50 +0700

    Initialize settings for Gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AndroidApplication-To-AAB"
+include(":app")
